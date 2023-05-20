b7bd3d0d6a901867551989e0ad9143ea3333c1f1
hikio-17 <fajri010217@gmail.com>
2023-05-20 08:37:46 -0700

    shared action

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,23 @@
 /**
  * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
  */
+
+import api from '../../utils/api';
+import { receiveUserActionCreator } from '../users/action';
+import { receiveTalksActionCreator } from '../talks/action';
+
+function asyncPopulateUsersAndTalks() {
+  return async (dispatch) => {
+    try {
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
+
+      dispatch(receiveUserActionCreator(users));
+      dispatch(receiveTalksActionCreator(talks));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export { asyncPopulateUsersAndTalks };
