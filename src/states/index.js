b9e25e8c94462023c2903c34c0bb84370a45ea67
hikio-17 <fajri010217@gmail.com>
2023-05-20 18:14:53 -0700

    create redux-store

--- src/states/index.js
+++ src/states/index.js
@@ -1,3 +1,21 @@
 /**
  * @TODO: Create Redux store
  */
+import { configureStore } from '@reduxjs/toolkit';
+import isPreloadReducers from './isPreload/reducer';
+import authUserReducers from './authUser/reducer';
+import usersReducer from './users/reducer';
+import talksReducer from './talks/reducer';
+import talkDetailReducers from './talkDetail/reducer';
+
+const store = configureStore({
+  reducer: {
+    isPreload: isPreloadReducers,
+    authUser: authUserReducers,
+    users: usersReducer,
+    talks: talksReducer,
+    talkDetail: talkDetailReducers,
+  },
+});
+
+export default store;
